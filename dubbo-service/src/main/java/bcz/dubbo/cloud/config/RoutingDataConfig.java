96d5f6a50500142ef46c3cfc57f551c13607ba3c
bingco <bingco.zn@gmail.com>
2019-09-20 11:39:07 +0800

    alibaba druid 数据库监控整合

--- dubbo-service/src/main/java/bcz/dubbo/cloud/config/RoutingDataConfig.java
+++ dubbo-service/src/main/java/bcz/dubbo/cloud/config/RoutingDataConfig.java
@@ -1,14 +1,25 @@
 package bcz.dubbo.cloud.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.support.spring.stat.BeanTypeAutoProxyCreator;
+import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
 import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Primary;
 
+import javax.servlet.Filter;
+import javax.servlet.Servlet;
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,13 +50,15 @@ public class RoutingDataConfig {
     }
 
     @Bean
-    public DataSource routingDataSource() {
+    public DataSource routingDataSource(@Qualifier("masterDataSource") DataSource masterDataSource,
+                                        @Qualifier("slaveDataSource_1") DataSource slaveDataSource_1,
+                                        @Qualifier("slaveDataSource_2") DataSource slaveDataSource_2) {
         Map<Object, Object> targetDataSources = new HashMap<>();
-        targetDataSources.put("MASTER", masterDataSource());
-        targetDataSources.put("SLAVE1", slaveDataSource_1());
-        targetDataSources.put("SLAVE2", slaveDataSource_2());
+        targetDataSources.put("MASTER", masterDataSource);
+        targetDataSources.put("SLAVE1", slaveDataSource_1);
+        targetDataSources.put("SLAVE2", slaveDataSource_2);
         BczRoutingDataSource dataSource = new BczRoutingDataSource();
-        dataSource.setDefaultTargetDataSource(masterDataSource());
+        dataSource.setDefaultTargetDataSource(masterDataSource);
         dataSource.setTargetDataSources(targetDataSources);
         DBHolder.setTotalSlaveDataSource(2);
         return dataSource;
@@ -55,5 +68,69 @@ public class RoutingDataConfig {
     @Bean("dataSource")
     public DataSourceProxy dataSource(@Qualifier("routingDataSource") DataSource dataSource) {
         return new DataSourceProxy(dataSource);
+    }
+
+    /**
+     * druidServlet注册
+     */
+    @Bean
+    public ServletRegistrationBean druidServletRegistration() {
+        ServletRegistrationBean<Servlet> registration = new ServletRegistrationBean<>(new StatViewServlet());
+        registration.addUrlMappings("/druid/*");
+        return registration;
+    }
+
+    /**
+     * druid监控 配置URI拦截策略
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>(new WebStatFilter());
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter(
+                "exclusions", "/static/*,*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid,/druid/*");
+        //用于session监控页面的用户名显示 需要登录后主动将username注入到session里
+        filterRegistrationBean.addInitParameter("principalSessionName", "username");
+        return filterRegistrationBean;
+    }
+
+    /**
+     * druid数据库连接池监控
+     */
+    @Bean
+    public DruidStatInterceptor druidStatInterceptor() {
+        return new DruidStatInterceptor();
+    }
+
+    @Bean
+    public JdkRegexpMethodPointcut druidStatPointcut() {
+        JdkRegexpMethodPointcut druidStatPointcut = new JdkRegexpMethodPointcut();
+        String patterns = "bcz.dubbo.cloud.service.*";
+        //可以set多个
+        druidStatPointcut.setPatterns(patterns);
+        return druidStatPointcut;
+    }
+
+    /**
+     * druid数据库连接池监控
+     */
+    @Bean
+    public BeanTypeAutoProxyCreator beanTypeAutoProxyCreator() {
+        BeanTypeAutoProxyCreator beanTypeAutoProxyCreator = new BeanTypeAutoProxyCreator();
+        beanTypeAutoProxyCreator.setTargetBeanType(DruidDataSource.class);
+        beanTypeAutoProxyCreator.setInterceptorNames("druidStatInterceptor");
+        return beanTypeAutoProxyCreator;
+    }
+
+    /**
+     * druid 为druidStatPointcut添加拦截
+     *
+     * @return
+     */
+    @Bean
+    public Advisor druidStatAdvisor() {
+        return new DefaultPointcutAdvisor(druidStatPointcut(), druidStatInterceptor());
     }
 }
